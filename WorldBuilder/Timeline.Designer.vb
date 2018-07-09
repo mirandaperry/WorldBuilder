96a5ad854133dbfe612b690b38a4d85d379ed59a
MikaylaRay44 <mikaylaray44@gmail.com>
2018-07-09 18:05:34 -0400

    Created a scrollable panel that I can draw the timeline in.

---
+++ WorldBuilder/Timeline.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Timeline
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MainPanel = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'MainPanel
+        '
+        Me.MainPanel.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MainPanel.AutoScroll = True
+        Me.MainPanel.AutoSize = True
+        Me.MainPanel.Location = New System.Drawing.Point(12, 12)
+        Me.MainPanel.Margin = New System.Windows.Forms.Padding(0)
+        Me.MainPanel.Name = "MainPanel"
+        Me.MainPanel.Size = New System.Drawing.Size(776, 426)
+        Me.MainPanel.TabIndex = 0
+        '
+        'Timeline
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MainPanel)
+        Me.Name = "Timeline"
+        Me.Text = "Timeline"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MainPanel As Panel
+End Class
---
+++ WorldBuilder/Timeline.vb
@@ -0,0 +1,7 @@
+﻿Public Class Timeline
+
+    ' This form has a Panel named 'MainPanel' that will scroll if needed.
+
+    ' TODO: Test drawing points and lines for the timeline mapping.
+
+End Class
